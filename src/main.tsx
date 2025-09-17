724972be5ce825bb530054b746c71dbaa9206ae4
Sabelli <sabellinho@gmail.com>
2025-09-17 14:57:26 -0300

    Criando rotas dentro de main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Error from './routes/Error/index.tsx'
+import Home from './routes/Home/index.tsx'
+import QuemSomos from './routes/QuemSomos/index.tsx'
+import SaudeAmiga from './routes/SaudeAmiga/index.tsx'
+import Oficinas from './routes/Oficinas/index.tsx'
+import Faq from './routes/Faq/index.tsx'
+import Contato from './routes/Contato/index.tsx'
 
+const router = createBrowserRouter([
+  {path:"/", element:<App/>, errorElement:<Error/>, children:[
+    {path:"/", element:<Home/>},
+    {path:"/quem-somos", element:<QuemSomos/>},
+    {path:"/saude-amiga", element:<SaudeAmiga/>},
+    {path:"/oficinas", element:<Oficinas/>},
+    {path:"/perguntas-frequentes", element:<Faq/>},
+    {path:"/contato", element:<Contato/>}
+  ]}
+])
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
