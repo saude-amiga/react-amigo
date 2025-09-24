a4d2e33aa14787a5ea14f792c81684e0eadf4a2f
BragaSoterasVinicius <viniciussoteras@gmail.com>
2025-09-24 16:39:07 -0300

    Adicionado banners no topo de Home

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,7 +1,21 @@
+
+import banner1 from '../../img/banner/banner1.jpg'
+import banner2 from '../../img/banner/banner2.jpg'
+import banner3 from '../../img/banner/banner3.jpg'
 export default function Home(){
     return(
-        <div>
-            <p>Home</p>
-        </div>
+        <body>
+            <div>
+                <div>
+                    <img src={banner1} alt="Casal de Idosos olhando um tablet"/>
+                </div>
+                <div>
+                    <img src={banner2} alt="Senhor de idade falando no telefone enquanto usa o notebook"/>
+                </div>
+                <div>
+                    <img src={banner3} alt="Idoso mexendo no tablet"/>
+                </div>
+            </div>
+        </body>
     )
 }
