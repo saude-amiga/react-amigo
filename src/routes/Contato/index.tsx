b84d40c49e2b9a13ae0b9afeea15af52a3fd445b
BragaSoterasVinicius <viniciussoteras@gmail.com>
2025-09-24 16:32:56 -0300

    tsx de Contatos reestruturado; apontamentos para email, telefone e whatsapp adicionados

--- src/routes/Contato/index.tsx
+++ src/routes/Contato/index.tsx
@@ -13,12 +13,15 @@ export default function Contato(){
                     <div>
                         <h1>Contato</h1>
                     </div>
-                    <li><a><img src={facebook} alt="Ícone do Facebook"/>Facebook</a></li>
-                    <li><a><img src={instagram} alt="Ícone do Instagram"/>Instagram</a></li>
-                    <li><a><img src={linkedin} alt="Ícone do LinkedIn"/>LinkedIn</a></li>
-                    <li><a><img src={telegram} alt="Ícone do Telegram"/>Telegram</a></li>
-                    <li><a><img src={tiktok} alt="Ícone do Tik Tok"/>TikTok</a></li>
-                    <li><a><img src={x_twitter} alt="Ícone do X / Twitter"/>X</a></li>
+                    <div>
+                        <li><a><img src={facebook} alt="Ícone do Facebook"/>Facebook</a></li>
+                        <li><a><img src={instagram} alt="Ícone do Instagram"/>Instagram</a></li>
+                        <li><a><img src={linkedin} alt="Ícone do LinkedIn"/>LinkedIn</a></li>
+                        <li><a><img src={telegram} alt="Ícone do Telegram"/>Telegram</a></li>
+                        <li><a><img src={tiktok} alt="Ícone do Tik Tok"/>TikTok</a></li>
+                        <li><a><img src={x_twitter} alt="Ícone do X / Twitter"/>X</a></li>
+                    </div>
+                    
                 </section>
             </main>
         </body>
