14a66470b015d26dba40c76582351b67b1dbd8a6
BragaSoterasVinicius <viniciussoteras@gmail.com>
2025-09-24 16:36:17 -0300

    tsx de Contatos reestruturado; apontamentos para email, telefone e whatsapp adicionados

--- src/routes/Contato/index.tsx
+++ src/routes/Contato/index.tsx
@@ -4,6 +4,9 @@ import x_twitter from '../../img/icones/x-twitter.png'
 import tiktok from '../../img/icones/tiktok.png'
 import telegram from '../../img/icones/telegram.png'
 import linkedin from '../../img/icones/linkedin.png'
+import email from '../../img/icones/mail.png'
+import tel from '../../img/icones/phone.png'
+import whats from '../../img/icones/whats.png'
 
 export default function Contato(){
     return(
@@ -12,16 +15,18 @@ export default function Contato(){
                 <section id="contato">
                     <div>
                         <h1>Contato</h1>
+                        <div>
+                            <li><a><img src={facebook} alt="Ícone do Facebook"/>Facebook</a></li>
+                            <li><a><img src={instagram} alt="Ícone do Instagram"/>Instagram</a></li>
+                            <li><a><img src={linkedin} alt="Ícone do LinkedIn"/>LinkedIn</a></li>
+                            <li><a><img src={telegram} alt="Ícone do Telegram"/>Telegram</a></li>
+                            <li><a><img src={tiktok} alt="Ícone do Tik Tok"/>TikTok</a></li>
+                            <li><a><img src={x_twitter} alt="Ícone do X / Twitter"/>X</a></li>
+                        </div>
+                        <li><a href="" target="_blank"><img src={email} alt="Ícone de E-mail"/>saudeamiga@email.com</a></li>
+                        <li><a href="" target="_blank"><img src={tel} alt="Ícone de Telefone"/>11 4444-4444</a></li>
+                        <li><a href="" target="_blank"><img src={whats} alt="Ícone de Whatsapp"/>11 99999-9999</a></li>
                     </div>
-                    <div>
-                        <li><a><img src={facebook} alt="Ícone do Facebook"/>Facebook</a></li>
-                        <li><a><img src={instagram} alt="Ícone do Instagram"/>Instagram</a></li>
-                        <li><a><img src={linkedin} alt="Ícone do LinkedIn"/>LinkedIn</a></li>
-                        <li><a><img src={telegram} alt="Ícone do Telegram"/>Telegram</a></li>
-                        <li><a><img src={tiktok} alt="Ícone do Tik Tok"/>TikTok</a></li>
-                        <li><a><img src={x_twitter} alt="Ícone do X / Twitter"/>X</a></li>
-                    </div>
-                    
                 </section>
             </main>
         </body>
