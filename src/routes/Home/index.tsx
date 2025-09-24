45a3142c3d98914a957a51cd20166c797ddf2611
BragaSoterasVinicius <viniciussoteras@gmail.com>
2025-09-24 16:56:52 -0300

    Adicionadas seções para acesso às oficinas e link para dúvidas frequentes

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -25,6 +25,19 @@ export default function Home(){
                         aplicativo e fique por dentro de tudo que o HC pode proporcionar</p>
                     <Link to='/saude-amiga'>Saiba mais ...</Link>
                 </section>
+
+                <section id="oficinas">
+                    <h1>Oficinas Presenciais</h1>
+                    <img src="../assets/img/oficina2.jpg"  alt="imagem oficina"/>
+                    <p>Em nossas oficinas, você encontrará dicas, tutoriais e conteúdos práticos para te ajudar a usar o celular, o computador e a internet de forma fácil e segura.</p>
+                    <Link to='/oficinas'>Saiba mais ...</Link>
+                </section>
+
+                <section id="homepergunta">
+                    <h1>Alguma dúvida?</h1>
+                    <Link to='/perguntas-frequentes'>Saiba mais ...</Link>
+                </section>
+
                 </div>
             </div>
         </body>
