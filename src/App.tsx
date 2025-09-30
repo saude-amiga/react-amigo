e505ea52fd7618e893a84899bafd4395986ab5e8
Rafaelkubagawa <143762533+Rafaelkubagawa@users.noreply.github.com>
2025-09-30 20:04:37 -0300

    Adicionada a função de voltar ao topo ao trocar de página

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Cabecalho from "./components/Cabecalho/Cabecalho";
 import Rodape from "./components/Rodape/Rodape";
+import ScrollToTop from './components/ScrollTop/ScrollTop';
 import '../global.css'
 
 export default function App(){
   return(
     <>
       <Cabecalho/>
+      <ScrollToTop/>
       <Outlet/>
       <Rodape/>
     </>
+
   )
 }
