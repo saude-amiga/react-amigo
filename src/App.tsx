3888512305a51fd1f1ad3e72d83ddb71c886094b
Rafaelkubagawa <143762533+Rafaelkubagawa@users.noreply.github.com>
2025-10-01 18:24:38 -0300

    Correção do footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import { Outlet } from "react-router-dom";
 import Cabecalho from "./components/Cabecalho/Cabecalho";
 import Rodape from "./components/Rodape/Rodape";
 import ScrollToTop from './components/ScrollTop/ScrollTop';
-import './global.css'
+import './global.css';
 
-export default function App(){
-  return(
-    <>
-      <Cabecalho/>
-      <ScrollToTop/>
-      <Outlet/>
-      <Rodape/>
-    </>
+export default function App() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Cabecalho />
+      <ScrollToTop />
 
-  )
+      <main className="flex-1">
+        <Outlet />
+      </main>
+
+      <Rodape />
+    </div>
+  );
 }
