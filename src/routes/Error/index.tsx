0e960880c5273678ac6c814869cc96abc5b07981
Rafaelkubagawa <143762533+Rafaelkubagawa@users.noreply.github.com>
2025-09-30 21:02:10 -0300

    Implementado UseNavigate na página "erro"

--- src/routes/Error/index.tsx
+++ src/routes/Error/index.tsx
@@ -1,19 +1,27 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function Error() {
+  const navigate = useNavigate();
+
+  const voltarParaHome = () => {
+    navigate("/");
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-800 px-4 text-center">
       <h1 className="text-6xl font-bold text-red-500 mb-4">404</h1>
       <p className="text-xl mb-6 max-w-md">
         Opa! Parece que essa página se perdeu no tempo... assim como a gente às vezes esquece onde deixou os óculos.
       </p>
-      <p className="text-xl mb-6 max-w-md">Mas tá tudo certo — você pode voltar com segurança para a página inicial.</p>
-      <Link
-        to="/"
+      <p className="text-xl mb-6 max-w-md">
+        Mas tá tudo certo — você pode voltar com segurança para a página inicial.
+      </p>
+      <button
+        onClick={voltarParaHome}
         className="px-6 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition duration-300"
       >
         Voltar para a página inicial
-      </Link>
+      </button>
     </div>
   );
 }
