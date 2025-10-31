260e5bd23ddfb2cafffe3665927d22d52eb03cc4
Sabelli <sabellinho@gmail.com>
2025-10-31 20:11:18 -0300

    feat(main): adicionando route de login

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,7 @@ import Faq from './routes/Faq/index.tsx'
 import Contato from './routes/Contato/index.tsx'
 import FaqResposta from './routes/FaqResposta/index.tsx'
 import './global.css'
+import Login from './routes/Login/index.tsx'
 
 const router = createBrowserRouter([
   {path:"/", element:<App/>, errorElement:<Error/>, children:[
@@ -20,7 +21,8 @@ const router = createBrowserRouter([
     {path:"/oficinas", element:<Oficinas/>},
     {path:"/perguntas-frequentes", element:<Faq/>},
     {path:"/perguntas-frequentes/:id", element:<FaqResposta/>},
-    {path:"/contato", element:<Contato/>}
+    {path:"/contato", element:<Contato/>},
+    {path:"/login", element:<Login/>}
   ]}
 ])
 createRoot(document.getElementById('root')!).render(
