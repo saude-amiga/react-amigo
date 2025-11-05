58049cab81394cab73fb7af485dcb1870a404b17
BragaSoterasVinicius <viniciussoteras@gmail.com>
2025-11-05 20:49:18 -0300

    fix(Agendamento/index.tsx): Corrigido abordagem da interface de agendamento
    Anteriormente estava incorreta pois levava em consideração que um cliente fizesse uso da interface, quando a mesma será utilizada por um funcionário

--- src/routes/Agendamento/index.tsx
+++ src/routes/Agendamento/index.tsx
@@ -31,7 +31,6 @@ export default function Agendamento(){
                 <h1>
                     Agendamento
                 </h1>
-                <p>Reserve seu horário e participe das oficinas presenciais de inclusão digital</p>
             </div>
             {agendado && (<div>
                 Seu agendamento foi realizado com sucesso!
