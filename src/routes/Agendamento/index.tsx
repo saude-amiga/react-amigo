50d926929d1be0bb8d480c8d67ec08f6d4c5b74e
BragaSoterasVinicius <viniciussoteras@gmail.com>
2025-11-05 19:48:52 -0300

    feature(Agendamento/index.tsx): Adicionado título h1, subtítulo e mensagem de confirmação de envio de agendamento ao topo da tela.

--- src/routes/Agendamento/index.tsx
+++ src/routes/Agendamento/index.tsx
@@ -1,11 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Agendamento(){
     useEffect(()=>{
         document.title = "Agendamento de Oficinas";
     },[])
 
+    const[agendado, setAgendado] = useState(false);
+
     return(
-        <main></main>
+        <main>
+            <div>
+                <h1>
+                    Agendamento de Oficinas
+                </h1>
+                <p>Reserve seu horário e participe das oficinas presenciais de inclusão digital</p>
+            </div>
+            {agendado && (<div>
+                Seu agendamento foi realizado com sucesso!
+            </div>)}
+        </main>
     );
 }
