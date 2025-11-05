74cd965018c389e88eda040f16a138e5dc6eb18a
BragaSoterasVinicius <viniciussoteras@gmail.com>
2025-11-05 20:01:28 -0300

    feature(Agendamento/index.tsx): adicionada validação e reset para envio do formulário

--- src/routes/Agendamento/index.tsx
+++ src/routes/Agendamento/index.tsx
@@ -1,12 +1,29 @@
 import { useEffect, useState } from "react";
+import type { AgendamentoFormData } from "../../types/agendamentoFormData";
+import { useForm } from "react-hook-form";
 
 export default function Agendamento(){
     useEffect(()=>{
         document.title = "Agendamento";
     },[])
 
+    const{
+        handleSubmit,
+        reset,
+        formState:{errors, isValid},
+    } = useForm<AgendamentoFormData>({
+        mode: "onChange",
+        reValidateMode: "onChange",
+    });
+
     const[agendado, setAgendado] = useState(false);
 
+    const onSubmit = (data: AgendamentoFormData) => {
+        console.log("Agendamento enviado: ", data);
+        setAgendado(true);
+        setTimeout(() => setAgendado(false), 4000);
+        reset(); 
+    }
     return(
         <main>
             <div>
@@ -17,7 +34,9 @@ export default function Agendamento(){
             </div>
             {agendado && (<div>
                 Seu agendamento foi realizado com sucesso!
-            </div>)}
+            </div>
+        )}
+        <form onSubmit={handleSubmit(onSubmit)}></form>
         </main>
     );
 }
