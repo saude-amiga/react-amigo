ebfbc489f9df7eddb583c0aba23cfaf742fd9ff2
Rafaelkubagawa <rafael.kubagawaramos@gmail.com>
2025-11-06 22:53:56 -0300

    Hotfix: Arrumando responsividade do menu

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -33,12 +33,12 @@ export default function Menu() {
     <div className="relative" ref={dropdownRef}>
       <button
         onClick={() => setIsOpen(!isOpen)}
-        className="md:hidden z-50 bg-[#29966a] text-white px-4 py-2 rounded-md font-semibold hover:bg-[#76b99d] transition">
+        className="lg:hidden z-50 bg-[#29966a] text-white px-4 py-2 rounded-md font-semibold hover:bg-[#76b99d] transition">
         Menu ▼
       </button>
 
       {isOpen && (
-        <div className="md:hidden absolute right-0 mt-2 w-64 max-h-[80vh] overflow-y-auto bg-white border border-[#76b99d] rounded-md shadow-lg z-40 menu-dropdown">
+        <div className="lg:hidden absolute right-0 mt-2 w-64 max-h-[80vh] overflow-y-auto bg-white border border-[#76b99d] rounded-md shadow-lg z-40 menu-dropdown">
           <nav className="p-4 space-y-2 text-[#194737] font-medium">
             <Link to="/">Home</Link>
             <Link to="/integrantes">Integrantes</Link>
@@ -50,7 +50,7 @@ export default function Menu() {
         </div>
       )}
 
-      <div className="hidden md:flex justify-end p-4 space-x-4 z-30 text-white font-medium menu-aberto">
+      <div className="hidden lg:flex justify-end p-4 space-x-4 z-30 text-white font-medium menu-aberto">
         <Link to="/">Home</Link>
         <Link to="/integrantes">Integrantes</Link>
         <Link to="/sobre">Sobre</Link>
